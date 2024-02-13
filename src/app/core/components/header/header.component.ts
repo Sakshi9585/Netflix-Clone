a3c44e3a1ce3cf988ae78ff783671d3a46ed8240
Sakshi-aher <sakshi.aher@winjit.com>
2024-02-13 10:53:12 +0530

    dynamic name fetched

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
+
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -11,4 +13,8 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
   @Input({required: true}) userImg: string='';
   navList = ["home", "Tv Shows", "News & Popular", "My List", "Browse by Language"]
+
+
+  name = JSON.parse(sessionStorage.getItem("loggedInUser")!).name;
+
 }
